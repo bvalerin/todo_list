7930a8a77d2764bada22cfc4fa9e002a8a07b710
Jose Carballo <jczord23@gmail.com>
2020-12-08 09:06:30 -0300

    se creo el entorno de desarrollo

---
+++ src/componets/Task.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Task = () => {
+  return (
+    <div>
+      <h1>tarea</h1>
+    </div>
+  );
+};
+
+export default Task;
---
+++ src/componets/menu/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1>menu laterar o sidebar </h1>
+    </div>
+  );
+};
+
+export default Sidebar;
