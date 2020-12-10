0f9a2ba223b84e0f8979429b9823e3284e765b3c
Arian <arian.gasolero@gmail.com>
2020-12-09 21:31:32 -0300

    Estilizamos el formulario del login, ya terminamos la parte visual

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./components/auth/Login";
 
+
 function App() {
   return (
     <div className="container_app">
       <Login />
+      
     </div>
   );
 }
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,18 +1,41 @@
 import React from "react";
-import "./styles.css";
+import "./login.css";
+import email from "../../images/email.svg";
+import pass from "../../images/pass.svg";
+import logo from "../../images/Logo.svg";
+import google from "../../images/google.svg";
 
 const Login = () => {
   return (
     <div className="login">
       <div className="container">
+        <div className="logo">
+          <img src={logo} alt="logo"/>
+        </div>
         <form action="">
-          <input type="text" name="email" placeholder="&#128231; Email" />
-          <input
-            type="numbre"
-            name="password"
-            placeholder="&#128273; Password"
-          />
+          <div className="inputLogin">
+            <img src={email} alt="Email"/>
+            <input type="email" name="email" placeholder="Email" />
+          </div>
+          <div className="inputLogin">
+            <img src={pass} alt="Password"/>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
         </form>
+        <button className="iniciarSesion">
+          Iniciar Sesion
+        </button>
+        <button className="iniciarSesionGoogle">
+          <img src={google} alt="Google"/>
+          Iniciar Sesion con Google
+        </button>
+        <hr className="linea"/>
+        <p>¿Eres nuevo? <a href="">¡Registrate!</a></p>
+        <a href="" className="olvidasteTuContraseña">¿Olvidaste tu constraseña?</a>
       </div>
     </div>
   );
