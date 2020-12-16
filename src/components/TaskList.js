7235f09e3fa3d833bbd57fad4d9d44ca27f1af4b
Luguito <gomezfrancisco.mac@gmail.com>
2020-12-16 13:15:52 -0500

    Revert "Se terminó el diseño con los estilos CSS del sidebar"
    This reverts commit 7979cafc65b2d8cd22522f87d2e9defb4666ebe7.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,23 +3,7 @@ import React from "react";
 const TaskList = () => {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
       <h1>lista de tareas</h1>
-=======
-      <h2>{title}</h2>
->>>>>>> parent of 7979caf... Se terminó el diseño con los estilos CSS del sidebar
-=======
-      <h1>lista de tareas</h1>
->>>>>>> parent of 2df4fb4... Merge remote-tracking branch 'origin/main' into dev
-=======
-      <h2>{title}</h2>
->>>>>>> parent of 7979caf... Se terminó el diseño con los estilos CSS del sidebar
-=======
-      <h2>{title}</h2>
->>>>>>> parent of 7979caf... Se terminó el diseño con los estilos CSS del sidebar
     </>
   );
 };
