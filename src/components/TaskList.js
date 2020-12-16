577da253abd6bfcc78d1d13635c37e69736e1f74
Luguito <gomezfrancisco.mac@gmail.com>
2020-12-16 13:50:30 -0500

    (EHN) Title add

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { ReactComponent as OptionsLogo} from "../images/options.svg";
+import { ReactComponent as OptionsLogo } from "../images/options.svg";
 
-const TaskList = () => {
+const TaskList = ({ title }) => {
   return (
     <div className="tasklist">
       <h4>{title}</h4>
       <div className="dropdown">
-          <button className="dropdown-button">
-              <OptionsLogo className="tasklist__logo-options" />
-          </button>
-          <ul className="dropdown-content">
-              <li>Editar</li>
-              <li>Eliminar</li>
-          </ul>
+        <button className="dropdown-button">
+          <OptionsLogo className="tasklist__logo-options" />
+        </button>
+        <ul className="dropdown-content">
+          <li>Editar</li>
+          <li>Eliminar</li>
+        </ul>
       </div>
     </div>
   );
