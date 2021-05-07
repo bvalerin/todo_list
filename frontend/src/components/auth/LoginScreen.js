4d7adc383f036e165ca54b50a1b49132594ca002
Andrés Carreño <brayanandre74@hotmail.com>
2021-05-07 17:41:50 -0500

    Creación de login y demás

--- frontend/src/components/auth/LoginScreen.js
+++ frontend/src/components/auth/LoginScreen.js
@@ -1,10 +1,105 @@
 
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import circleImage from '../../images/figure-images/circle.svg';
+import triangleImage from '../../images/figure-images/triangle.svg';
+import hexagonalImage from '../../images/figure-images/hexagonal.svg';
+import squareImage from '../../images/figure-images/square.svg';
+
+import facebookIcon from '../../images/socialmedia-icons/facebook.svg';
+import twitterIcon from '../../images/socialmedia-icons/twitter.svg';
+import googleIcon from '../../images/socialmedia-icons/google.svg';
+
+import lockIcon from '../../images/input-icons/lock.svg';
+import messageIcon from '../../images/input-icons/message.svg';
 
 export const LoginScreen = () => {
     return (
-        <div>
-            <h1>Login Screen</h1>
+        <div className="auth__login container">
+
+            <div className="login__box-content">
+
+                <p className="auth__title"> Do Task</p>
+
+                <div className="login__title">
+                    <h2>Inicia sesión</h2>
+
+                    <nav className="socialmedia__navbar">
+                        <li><img src={ facebookIcon } alt="Facebook Icon" /></li>
+                        <li><img src={ googleIcon } alt="Facebook Icon" /></li>
+                        <li><img src={ twitterIcon } alt="Facebook Icon" /></li>    
+                    </nav>
+                </div>
+
+                <form className="auth__form">
+                    <p>o use su cuenta para ingresar:</p>
+
+                    <div className="input__form-box">
+                        <div className="input__img-box">
+                            <img src={ messageIcon } alt="Message Icon" />
+                        </div>
+                        <input 
+                            type="text"
+                            className="input__auth"
+                            placeholder="Mail"
+                            name="email" 
+                            autoComplete="off"
+                        />
+                    </div>
+
+                    <div className="input__form-box">
+                        <div className="input__img-box">
+                            <img src={ lockIcon } alt="Lock Icon" />
+                        </div>
+                        <input 
+                            type="password"
+                            className="input__auth"
+                            placeholder="Contraseña"
+                            name="password" 
+                            autoComplete="off"
+                        />
+                    </div>
+
+                    
+                    <Link 
+                        className="btn-link"
+                        to="/auth/register"
+                    >
+                        ¿Olvidaste tu contraseña?
+                    </Link>
+
+                    <button
+                        className="btn btn-primary"
+                    >
+                        Iniciar Sesión
+                    </button>
+                </form>
+            </div>
+
+            <div className="login__box-message">
+                <h3>¡Hola, amigo/a!</h3>
+                <p>Si no tienes cuenta, registrate</p>
+
+                <Link
+                    to="/auth/register"
+                    className="btn btn-outline"
+                >
+                    Registrarse
+                </Link>
+
+                <div className="login__figures">
+                    <img src={ circleImage } alt="Circle Icon" />
+                    <img src={ circleImage } alt="Circle Icon" />
+                    <img src={ triangleImage } alt="Triangle Icon" />
+                    <img src={ triangleImage } alt="Triangle Icon" />
+                    <img src={ hexagonalImage } alt="Hexagonal Icon" />
+                    <img src={ hexagonalImage } alt="Hexagonal Icon" />
+                    <img src={ squareImage } alt="Hexagonal Icon" />
+                    <img src={ squareImage } alt="Hexagonal Icon" />
+                </div>
+            </div>
+
         </div>
     )
 }
